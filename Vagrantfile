1c269a69d3c6465b40a1a668999f6c7f5117d72f
Ivan <ivan.daum@gmail.com>
2015-05-27 15:45:56 +0200

    feat(scripts): first push

---
+++ Vagrantfile
@@ -0,0 +1,10 @@
+VAGRANTFILE_API_VERSION = "2"
+
+Vagrant.configure(VAGRANTFILE_API_VERSION) do |config|
+  config.vm.box = "hashicorp/precise32"
+  config.vm.hostname = "zombicide-online"
+  config.vm.provision :shell, path: "install.sh"
+  config.vm.network "private_network", ip: "192.168.100.10"
+  config.vm.network "forwarded_port", guest: 3000, host: 3000
+  config.vm.synced_folder "www/", "/var/www/"
+end
---
+++ install.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+
+# Start and update vagrant
+echo -e "\n--- Install Vagrant ---\n"
+sudo apt-get update
+sudo apt-get install -y git git-core curl zlib1g-dev build-essential libssl-dev libreadline-dev libyaml-dev libsqlite3-dev sqlite3 libxml2-dev libxslt1-dev libcurl4-openssl-dev python-software-properties libffi-dev
+
+# Install RVM
+echo -e "\n--- Install RVM  ---\n"
+su - vagrant -c 'curl -sSL https://get.rvm.io | bash -s'
+su - vagrant -c 'source ~/.rvm/scripts/rvm'
+su - vagrant -c 'echo "source ~/.rvm/scripts/rvm" >> ~/.bashrc'
+
+# Install Ruby
+echo -e "\n--- Install Ruby latest version using RVM ---\n"
+su - vagrant -c 'rvm install ruby'
+
+# Install Rails 
+echo -e "\n--- Install Rails ---\n"
+su - vagrant -c "gem install rails --no-ri --no-rdoc"
+
+echo -e "\n--- Install MySQL ---\n"
+apt-get install debconf-utils -y > /dev/null
+debconf-set-selections <<< "mysql-server mysql-server/root_password password root"
+debconf-set-selections <<< "mysql-server mysql-server/root_password_again password root"
+sudo apt-get install -y mysql-server mysql-client libmysqlclient-dev > /dev/null
+
+echo -e "\n--- Create Database ---\n"
+sudo mysql -h localhost -u root -proot -e "CREATE DATABASE zombicide_dev;"
